cd727cfcf53f891da38b7ba87d4697524d30ba1d
mann-lean <mannlearn210@gmail.com>
2026-03-05 18:11:25 +0530

    changes for github action

--- nycTaxiProject/app.py
+++ nycTaxiProject/app.py
@@ -47,4 +47,5 @@ def index():
 
 if __name__ == '__main__':
     debug_mode = os.environ.get("FLASK_DEBUG", "false").lower() == "true"
+    
     app.run(host="0.0.0.0", port=8080, debug=debug_mode)
